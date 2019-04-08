fff0d2b7c7b87ce71d421e251e06a531a99a9d8d
KennethA2001 <31477599+KennethA2001@users.noreply.github.com>
2019-04-08 10:32:08 -0400

    Add files via upload

--- quizlet.py
+++ quizlet.py
@@ -1,22 +1,21 @@
 def header():
-    print("Welcome to Kenneth's Quizlet.  This program is meant to help you study for your upcoming assesmen")
+    print("Welcome to Kenneth's Quizlet.  This program is meant to help you study for your upcoming assesment.")
 
 
 def get_num_terms():
 
-    terms = input("Enter the number of terms you would like to study ")
+    term = input("Enter the number of terms you would like to study ")
+    terms = int(term)
     
+    a = str(input("Please enter the first term you would like to study"))
+    terms -= 1
 
-    if terms.isnumeric():
-        a = str(input("Please enter the first term you would like to study"))
-
-        if a is str:
-            terms -= 1
-            if terms > 0:
-                print(terms)
-            
-    else:
-        print(" Make sure you enter a number of terms ")
+        
+     if terms == 0:
+        print("Enter the defintion for the term")
+    elif terms > 0:
+        print(a)
+    
 
 
                 
