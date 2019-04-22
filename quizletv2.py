fd849be2652092e7e10e69600ef66cd5da2a9399
KennethA2001 <31477599+KennethA2001@users.noreply.github.com>
2019-04-22 10:31:55 -0400

    Add files via upload

--- quizletv2.py
+++ quizletv2.py
@@ -4,6 +4,8 @@ def header():
 
 def get_terms():
 
+    global term, terms, termList, list_terms
+
 
     term = input("Enter the number of terms you would like to study ")
     terms = int(term)
@@ -17,11 +19,33 @@ def get_terms():
     print("Here is your terms list")
     print(termList)
 
+    for list_terms in termList:
+        definition = input("Enter the definition for " + list_terms )
+
+def quiz(list_terms,termList):
+
+    points = 0
+
+    missed_words = []
+
+    print(" Lets Quiz!!")
+
+    streak = 0
+
     for list_term in termList:
-        definition = input("Enter the definition for " + list_term)
+        guess= str(input("Input the definition of" + list_term))
+        if guess == definition:
+            points += 1
+            streak += 1
+        else:
+            missed_words.append(defintion)
+            streak = 0
+    return points
+
+
      
         
                 
 header()
 get_terms()
-
+quiz(list_terms, termList)
