5280d83789821e837f47d369dab88416d0215776
KennethA2001 <31477599+KennethA2001@users.noreply.github.com>
2019-05-01 08:05:22 -0400

    Add files via upload

--- quizletv2.py
+++ quizletv2.py
@@ -4,28 +4,27 @@ def header():
 
 def get_terms():
 
-    global term, terms, termList, list_terms, definition, points
+    global term, terms, termList, list_terms, definition, streak, missed_words, points, def_list
 
 
     term = input("Enter the number of terms you would like to study ")
-    num_terms = int(term)
+    terms = int(term)
 
     termList = []
-
     def_list = []
 
-    def zip(termList, def_list):
-        
-        while len(termList) < num_terms:
-            list_terms = input("Enter terms you would like to study: ")
-            termList.append(list_terms)
-            print(termList)
-        print("Here is your terms list")
-        print(termList)
 
-        for list_terms in termList:
-            definition = input("Enter the definition for " + list_terms )
-            def_list.append(definition)
+    while len(termList) < terms:
+        list_terms = input("Enter terms you would like to study: ")
+        termList.append(list_terms)
+        print(termList)
+    print("Here is your terms list")
+    print(termList)
+
+    for list_terms in termList:
+        definition = input("Enter the definition for " + list_terms + " " )
+        def_list.append(definition)
+
 
 def quiz(list_terms,termList):
 
@@ -38,29 +37,30 @@ def quiz(list_terms,termList):
     streak = 0
 
     for list_term in termList:
-        
+
         guess= str(input("Input the definition of " + list_term))
 
-        if guess == definition:
-            points += 1
-            #streak += 1
-        #else:
-            #missed_words.append(definition)
-            #streak = 0
+        for n1,n2 in zip(termList, def_list):
+
+            if guess == definition:
+                points += 1
+                streak += 1
+                                
+            if streak >= 2:
+                if streak % 2 == 1:
+                    print("Wow, you must be a genius")
+                else:
+                    print("Keep going, you're doing great")
+            elif streak == 1:
+                print("You're almost there")
+
+            else:
+                missed_words.append(definition)
+                streak = 0
             
-        #if streak >= 2:
-            #if streak % 2 == 1:
-                #print("Wow, you must be a genius")
-            #else:
-                #print("Keep going, you're doing great")
-        #elif streak == 1:
-            #print("You're almost there")
-        #else:
-            #print("Keep studying, I know you got this")
 
     print(" Game over. You got, " + str(points) + " outta " + term)
-    return missed_words
-
+    print(missed_words)
 
 
      
